b5dab42d505f4d77146b536d39485f4c77cd6549
soukaina g <soukainaguezour@gmail.com>
2024-03-02 09:37:34 +0000

    players and chatrooms controller don

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -1,2 +1,11 @@
 class ChatroomController < ApplicationController
+  def create
+    @chatroom = Chatroom.new(chatroom_params)
+    @chatroom.game = @game
+    if @chatroom.save
+      redirect_to chatroom_path(@chatroom)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
 end
