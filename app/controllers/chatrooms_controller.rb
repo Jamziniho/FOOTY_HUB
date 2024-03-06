eb133528ae338401031c45d99ec78bd52f457d24
AbdulRrahmaan Omar <abdulrrahmaanwork@gmail.com>
2024-03-06 15:59:37 +0000

    Created chatroom without refresh

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -1,15 +1,6 @@
-class ChatroomController < ApplicationController
-  def create
-    @chatroom = Chatroom.new(chatroom_params)
-    @chatroom.game = @game
-    if @chatroom.save
-      redirect_to chatroom_path(@chatroom)
-    else
-      render :new, status: :unprocessable_entity
-    end
-  end
-
+class ChatroomsController < ApplicationController
   def show
     @chatroom = Chatroom.find(params[:id])
+    @message = Message.new
   end
 end
